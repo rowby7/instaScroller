df525115d84581d665e81bbcab1352f826eb6911
rowby7 <rowby_villanueva@outlook.com>
2025-07-14 14:27:42 -0400

    Initial Commit

---
+++ instaScroller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaScroller
+//
+//  Created by Rowby Villanueva on 7/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ instaScrollerTests/instaScrollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  instaScrollerTests.swift
+//  instaScrollerTests
+//
+//  Created by Rowby Villanueva on 7/14/25.
+//
+
+import Testing
+@testable import instaScroller
+
+struct instaScrollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
